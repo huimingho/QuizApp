e7c00cde9dffec57f854785620fbdf2961438c90
huimingho <77143361+huimingho@users.noreply.github.com>
2021-11-29 12:06:26 +0800

    Update QuizElement.js

--- QuizElement.js
+++ QuizElement.js
@@ -1,14 +1,19 @@
 import React from "react"
 
 export default function QuizElement(props) {
-    
     const answerElement = props.randAnsArr.map(answer => {
         return <button 
                     key={answer}
                     id={answer} 
                     className="quiz-options" 
                     onClick={() => props.handleClick(props.question, answer)}
-                    style={{backgroundColor: props.selectedans === answer ? "#D6DBF5" : "white"}}
+                    style={{
+                        backgroundColor: (props.ischecked ? 
+                            (props.iscorrect ? 
+                            (props.selectedans === answer ? "#94D7A2" : "white"): (props.selectedans === answer ? "#F8BCBC" : "white")) : 
+                            (props.selectedans === answer ? "#D6DBF5" : "white")
+                            )
+                        }}
                 >
                     {answer}
                 </button>
